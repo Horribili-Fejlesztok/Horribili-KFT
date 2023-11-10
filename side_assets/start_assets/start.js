adff630e550ded144f0d13fd6947dcd1e0897e52
kiss-csongor04 <kisscsongor@students.jedlik.eu>
2023-11-10 11:12:52 +0100

    Version 1.9.3 Videó és gombok hozzá kész

--- side_assets/start_assets/start.js
+++ side_assets/start_assets/start.js
@@ -1,4 +1,18 @@
+var video = document.getElementById("video")
+var betekintes = document.getElementById("betekintes")
+var kitekintes = document.getElementById("kitekintes")
+var kitekintes2 = document.getElementById("kitekintes2")
+
 function showVideo() {
-    const element = document.getElementById("video");
-    element.style.display = "inline-block";
-  }
+    video.style.display = "flex"
+    betekintes.style.display = "none"
+    kitekintes.style.display = "block"
+    kitekintes2.style.display = "block"
+}
+
+function unshowVideo() {
+    video.style.display = "none"
+    betekintes.style.display = "block"
+    kitekintes.style.display = "none"
+    kitekintes2.style.display = "none"
+}
